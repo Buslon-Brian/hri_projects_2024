affbb2e287d798452210f502080b8301dd1fb1f0
Buslon-Brian <buslon.aact@gmail.com>
2024-09-20 10:10:50 -0700

    test

--- week3/scripts/joint_demo.py
+++ week3/scripts/joint_demo.py
@@ -2,7 +2,6 @@
 # license removed for brevity
 import rospy
 import math
-import time
 from sensor_msgs.msg import JointState
 
 
@@ -10,13 +9,13 @@ def initialize(js):
     js.header.stamp = rospy.get_rostime()
     js.header.frame_id="Torso"
 
-    js.name.append("HeadYaw")
-    js.name.append("HeadPitch")
-    js.name.append("LHipYawPitch")
-    js.name.append("LHipRoll")
-    js.name.append("LHipPitch")
-    js.name.append("LKneePitch")
-    js.name.append("LAnklePitch")
+    js.name.append("HeadYaw") #0
+    js.name.append("HeadPitch") #1
+    js.name.append("LHipYawPitch") #2
+    js.name.append("LHipRoll") #3
+    js.name.append("LHipPitch") #5
+    js.name.append("LKneePitch") #6
+    js.name.append("LAnklePitch") #7
     js.name.append("LAnkleRoll")
     js.name.append("LShoulderPitch")
     js.name.append("LShoulderRoll")
@@ -37,6 +36,13 @@ def initialize(js):
     js.name.append("RWristYaw")
     js.name.append("RHand")
 
+    js.position.append(0) 
+    js.position.append(0)
+    js.position.append(0) 
+    js.position.append(0)
+    js.position.append(0)
+    js.position.append(0) 
+    js.position.append(0) 
     js.position.append(0)
     js.position.append(0)
     js.position.append(0)
@@ -56,31 +62,16 @@ def initialize(js):
     js.position.append(0)
     js.position.append(0)
     js.position.append(0)
-    js.position.append(0)
-    js.position.append(0)
-    js.position.append(0)
-    js.position.append(0)
-    js.position.append(0)
-    js.position.append(0)
-    js.position.append(0)
-
-def headback(js):
-    js.position[0] = math.radians(180)
-
-def headleft(js):
-    js.position[0] = math.radians(90)
+    
 
 def talker(pub, rate, js):
-    headleft(js)
     rate.sleep()
     pub.publish(js)
     
-
-def talker2(pub, rate, js):
-    headback(js)
+def LArmPitch(pub, rate, js, deg):
+    js.position[js.name.index("LShoulderPitch")] = math.radians(deg)
     rate.sleep()
     pub.publish(js)
-    
 
 if __name__ == '__main__':
     pub = rospy.Publisher('joint_states', JointState, queue_size=10)
@@ -90,7 +81,10 @@ if __name__ == '__main__':
     
     try:
         initialize(js)
-        talker(pub, rate, js)
-        talker2(pub, rate, js)
+        LArmPitch(pub, rate, js, 0)
+        LArmPitch(pub, rate, js, 90)
+
+        
+        
     except rospy.ROSInterruptException:
         pass
