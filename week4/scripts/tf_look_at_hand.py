562535d3e55c8012ba4b20deaefeb930489aaa66
Buslon-Brian <buslon.aact@gmail.com>
2024-10-16 13:31:59 -0700

    extremely primitive hand tracking

--- week4/scripts/tf_look_at_hand.py
+++ week4/scripts/tf_look_at_hand.py
@@ -7,25 +7,47 @@ from sensor_msgs.msg import JointState
 import geometry_msgs.msg
 
 # this is based on the ROS tf2 tutorial: http://wiki.ros.org/tf2/Tutorials/Writing%20a%20tf2%20listener%20%28Python%29
+ 
+prev_pitch = 0
 
 def callback(data):
-    print(data)
+    overwrite = JointState()
+    overwrite.header = data.header
+    overwrite.name = list(data.name)
+    overwrite.position = list(data.position)
+    # pub.publish(overwrite) #probably causes head stuttering
+    # rate.sleep() #section can be delayed unless Rate is large
+    # print("why so delayed?") 
+
+    try:
+        trans = tfBuffer.lookup_transform('Head', 'LFinger13_link', rospy.Time(0))
+        hf_pitch = math.atan2(trans.transform.translation.x, trans.transform.translation.z)
+        hf_yaw = math.atan2(trans.transform.translation.y, trans.transform.translation.x)
+        overwrite.position[overwrite.name.index("HeadPitch")] = math.radians(hf_pitch*40)
+        overwrite.position[overwrite.name.index("HeadYaw")] = math.radians(hf_yaw*40)
+
+        pub.publish(overwrite)
+        print(overwrite)
+        rate.sleep()
+
+    except (tf2_ros.LookupException):
+        print("Failed to get transform, skipping")
+        pub.publish(overwrite)
+        return    
 
 if __name__ == '__main__':
-    rospy.Subscriber('joint_states_input', JointState, callback)
-    pub = rospy.Publisher('joint_states', JointState, queue_size=10)
     rospy.init_node('tf2_look_at_hand')
+    
+    
     tfBuffer = tf2_ros.Buffer()
     listener = tf2_ros.TransformListener(tfBuffer)
+    
+    pub = rospy.Publisher('joint_states', JointState, queue_size=10)  
+    rospy.Subscriber('joint_states_input', JointState, callback)    
+    rate = rospy.Rate(100)
 
-
-    rate = rospy.Rate(10.0)
-    while not rospy.is_shutdown():
-        try:
-            trans = tfBuffer.lookup_transform('Head', 'l_gripper', rospy.Time())
-        except (tf2_ros.LookupException, tf2_ros.ConnectivityException, tf2_ros.ExtrapolationException):
-            rate.sleep()
-            continue
-
-        print("trans: x: %f y: %f z: %f", trans.transform.translation.x, trans.transform.translation.y, trans.transform.translation.z)
-        rate.sleep()
+    init = JointState() #doesn't fix transform issue
+    pub.publish(init)
+    rate.sleep()
+    
+    rospy.spin()
